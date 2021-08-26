f8d0b5fb2ebc7d368d54e26ce30e9cd59117834d
luckbox314 <lucas.muoz@uc.cl>
2021-08-26 15:30:00 -0400

    Animation implemented

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    // Movement vectors
     Vector3 horizontal;
     Vector3 vertical;
+    Vector3 currentDirection;
     Vector3 prevDirection;
+    
+
+    // Animation components
+    public Animator animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,29 +24,40 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        currentDirection = new Vector3();
         horizontal = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, 0.0f);
         vertical = new Vector3(0.0f, Input.GetAxisRaw("Vertical"), 0.0f);
+
+        // Direction logic
         if (horizontal != new Vector3() && vertical == new Vector3())
         {
-            transform.position = transform.position + horizontal * Time.deltaTime;
+            currentDirection = horizontal;
             prevDirection = horizontal;
         }
         else if (horizontal == new Vector3() && vertical != new Vector3())
         {
-            transform.position = transform.position + vertical * Time.deltaTime;
+            currentDirection = vertical;
             prevDirection = vertical;
         }
         else
         {
             if (prevDirection == vertical)
             {
-                transform.position = transform.position + horizontal * Time.deltaTime;
+                currentDirection = horizontal;
             }
             else if (prevDirection == horizontal)
             {
-                transform.position = transform.position + vertical * Time.deltaTime;
+                currentDirection = vertical;
             }
         }
-        
+
+        // Movement
+        transform.position = transform.position + currentDirection * Time.deltaTime;
+
+        // Animation logic
+        animator.SetInteger("Horizontal", (int) currentDirection.x);
+        animator.SetInteger("Vertical", (int) currentDirection.y);
+        Debug.Log($"{(int) currentDirection.x}, {(int) currentDirection.y}");
+
     }
 }
